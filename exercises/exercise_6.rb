3726bda10ee20f268b92a2bf7fdc0195ef81143e
Slo <temiora@MacBook-Air.local>
2023-11-03 16:32:53 -0500

    establish a one-to-many association between the Store and Employee models

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,14 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(
+  first_name: "John",
+  last_name: "Doe",
+  hourly_rate: 60
+)
+
+@store2.employees.create(
+  first_name: "Jane",
+  last_name: "Smith",
+  hourly_rate: 55
+)

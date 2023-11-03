2768053ab2604964c7e15d71545cdc83d47562b0
Slo <temiora@MacBook-Air.local>
2023-11-03 16:26:03 -0500

    calaculating the number of stores generating $1 million or more in annual sales

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Company Revenue: $#{total_revenue}"
+
+average_revenue = Store.average(:annual_revenue)
+puts "Average Annual Revenue for All Stores: $#{average_revenue}"
+
+high_revenue_stores = Store.where("annual_revenue >= ?", 1000000).size
+puts "Number of Stores Generating $1M or More: #{high_revenue_stores}"
